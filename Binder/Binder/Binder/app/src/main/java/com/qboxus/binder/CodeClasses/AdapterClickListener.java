7e6aacc7a9890880a2cfe160bfb21ef4f92c73bd
lalala <buadren@eclipso.eu>
2020-10-21 03:04:59 -0700

    changed all base on document
    document

--- Binder/Binder/Binder/app/src/main/java/com/qboxus/binder/CodeClasses/AdapterClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.qboxus.binder.CodeClasses;
-
-import android.view.View;
-
-public interface AdapterClickListener {
-    void onItemClick(int pos, Object item, View view);
-
-    void onLongItemClick(int pos, Object item, View view);
-}
---
+++ Binder/Binder/Binder/app/src/main/java/com/qboxus/pepinder/CodeClasses/Callback.java
@@ -0,0 +1,10 @@
+package com.qboxus.binder.CodeClasses;
+
+/**
+ * Created by AQEEL on 4/4/2019.
+ */
+
+public interface Callback {
+
+    void Responce(String resp);
+}
